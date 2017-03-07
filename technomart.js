80d52343dd38872cb22bafdcbc7d3087059d6971
EduardSat <eduardus424@gmail.com>
2017-03-06 18:18:32 -0800

    fixed catalog page

--- technomart.js
+++ technomart.js
@@ -1,3 +1,12 @@
-jQuery('document').ready(function(){
-jQuery('body').append('<a href="http//google.com">Перейти в гугл</a>')
-}); 
+var link = document.querySelector(".companies-contacts__btn");
+var popup = document.querySelector(".contacts-if__loose");
+var close = document.querySelector(".if-loose__close");
+
+link.addEventListener("click", function (event) {
+  event.preventDefault();
+  popup.classList.add("contacts-if__loose");
+});
+close.addEventListener("click", function (event) {
+      event.preventDefault();
+      popup.classList.remove("contacts-if__loose"); 
+      });
