086a3de56f40513f37d8fe4a3ef54c92d3d1a5d8
EduardSat <eduardus424@gmail.com>
2017-03-26 14:49:49 -0700

    added js file

--- js/technomart.js
+++ js/technomart.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
-  $('.category-gallery').slick({
-    setting-name: setting-value
-  });
-});
+var link = document.querySelector(".companies-contacts__btn");
+var popup = document.querySelector(".companies-contacts__form");
+var close = document.querySelector(".if-loose__close");
+
+link.addEventListener("click", function(event) {
+event.preventDefault();
+popup.classList.add("contacts-form-show");
+ });
+ 
+ close.addEventListener("click" , function(event) {
+   event.preventDefault();
+   popup.classList.remove("contacts-form-show");
+ });
